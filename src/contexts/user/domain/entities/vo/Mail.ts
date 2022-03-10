7fb75799dac235b42b2cc25884e213e5067f9ca9
Marc Rodriguez <marc@doblefilo.dev>
2022-03-10 18:25:08 +0100

    feat: add mail ValueObject

---
+++ src/contexts/user/domain/entities/vo/Mail.ts
@@ -0,0 +1,23 @@
+class Mail {
+  private _value: string;
+
+  constructor(value: string) {
+    this.create(value);
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  private create(value: string) {
+    const regex = new RegExp(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+
+    if (regex.test(value)) {
+      this._value = value;
+    }
+  }
+}
+
+export default Mail;
