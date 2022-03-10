2c140aaa20c341ee2f7b9cf9a406486b06c5a5d5
Marc Rodriguez <56035187+marc-doblefilo@users.noreply.github.com>
2022-03-10 15:13:21 +0100

    feat: add swagger module for autodocumentation (#1)
    Co-authored-by: Marc Rodríguez <marc@doblefilo.dev>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('UCOCamp Users Microservice')
+    .setDescription('Users microservice API. ')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
