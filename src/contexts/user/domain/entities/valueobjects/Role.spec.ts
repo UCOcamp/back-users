1b02314a3e0ff75b7cdf5c9f8f058bb8fc3a62a3
Marc Rodriguez <marc@doblefilo.dev>
2022-03-11 13:12:36 +0100

    feat: add Role ValueObject

---
+++ src/contexts/user/domain/entities/valueobjects/Role.spec.ts
@@ -0,0 +1,25 @@
+import Role from './Role';
+
+describe('RoleVO', () => {
+  let role: Role;
+
+  it('should return CREATOR role', () => {
+    role = new Role('CREATOR');
+
+    expect(role.value).toBe('CREATOR');
+  });
+
+  it('should return STUDENT role', () => {
+    role = new Role('STUDENT');
+
+    expect(role.value).toBe('STUDENT');
+  });
+
+  it('should return an Instance of Error', () => {
+    try {
+      new Role('TEST');
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+    }
+  });
+});
---
+++ src/contexts/user/domain/entities/valueobjects/Role.ts
@@ -0,0 +1,22 @@
+class Role {
+  private _value: string;
+  private _roles = ['CREATOR', 'STUDENT'];
+
+  constructor(value: string) {
+    this.create(value);
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  private create(value: string) {
+    if (this._roles.includes(value) == false) {
+      throw new Error('This role is not valid.');
+    }
+
+    this._value = value;
+  }
+}
+
+export default Role;
